143b76302e6f036b580cdc7663e3462fb0f638bf
esetiadi <ellysetiadi@gmail.com>
2018-12-09 13:20:12 -0600

    fixed jumbotron
    use javascript

---
+++ javascript.js
@@ -0,0 +1,10 @@
+document.getElementById("grid-button").addEventListener("click",function(e) { $('#iphone-grid').insertBefore($('.perfect-jumbotron').children()[0]);
+})
+
+document.getElementById("calendar-button").addEventListener("click",function(e) { $('#calendar').insertBefore($('.perfect-jumbotron').children()[0]);})
+
+document.getElementById("kanban-button").addEventListener("click",function(e) { $('#kanban').insertBefore($('.perfect-jumbotron').children()[0]);})
+
+document.getElementById("gallery-button").addEventListener("click",function(e) { $('#gallery').insertBefore($('.perfect-jumbotron').children()[0]);})
+
+document.getElementById("form-button").addEventListener("click",function(e) { $('#form').insertBefore($('.perfect-jumbotron').children()[0]);})
